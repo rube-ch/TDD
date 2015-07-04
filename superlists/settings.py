f8bda10fb31160f826844d08f60bfcbd614d9ba7
rube-ch <rubech@gmail.com>
2015-07-03 19:33:38 -0500

    Final del capitulo 6, dividimos urls generales y de lists

--- superlists/settings.py
+++ superlists/settings.py
@@ -47,9 +47,26 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.security.SecurityMiddleware',
 )
 
 ROOT_URLCONF = 'superlists.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
 
 WSGI_APPLICATION = 'superlists.wsgi.application'
 
--- superlists/urls.py
+++ superlists/urls.py
@@ -1,9 +1,9 @@
 from django.conf.urls import include, url
 from lists import views as list_views
+from lists import urls as list_urls
 
 urlpatterns = [
     url(r'^$', list_views.home_page, name='home'),
-    url(r'^lists/new$', list_views.new_list, name='new_list'),
-    url(r'^lists/(\d+)/$', list_views.view_list, name='view_list'),
-    url(r'^lists/(\d+)/add_item$', list_views.add_item, name='add_item'),
+    url(r'^lists/', include(list_urls)),
+    # url(r'^admin/', include(admin.site.urls)),
 ]
